67aebc1099a6c7c75288bd8d37228b17c6819bef
AmirHosein <awir.software@gmail.com>
2023-04-28 21:21:44 +0430

    set router

--- src/Router/Router.ts
+++ src/Router/Router.ts
@@ -1,7 +1,9 @@
-import { Express } from "express";
+import { Express, urlencoded } from "express";
+import morgan from "morgan";
 
 export default class Router{
     public init(app: Express): void{
-        app.use()
+        app.use(morgan('tiny'));
+        app.use(urlencoded({extended: true}));
     }
 }
