a08579edf3553a7e4c9125a2ec6a14a5fc44d657
AmirHosein <awir.software@gmail.com>
2023-04-28 21:39:51 +0430

    set get by id user controller

--- src/Controller/UserController.ts
+++ src/Controller/UserController.ts
@@ -2,10 +2,17 @@ import { Request, Response } from "express";
 import {User} from "../Model/User";
 
 export default class UserController{
-    async getAll(req: Request, res: Response){
+    async getAll(req: Request, res: Response): Promise<Object>{
         const users = await User.find();
-        if(!User) return res.status(404).send('Not Found');
+        if(!users) return res.status(404).send('Not Found');
 
         return res.send(users);
+    }
+
+    async getById(req: Request, res: Response): Promise<Object>{
+        const user = await User.findById(req.params.id);
+        if(!user) return res.status(404).send('Not Found');
+
+        return res.send(user);
     }
 }
