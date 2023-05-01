3103c0b810d9fc6ae7e53f7fa4de097670dbf7e8
AmirHosein <awir.software@gmail.com>
2023-05-02 00:47:00 +0430

    add edit user

--- src/Controller/UserController.ts
+++ src/Controller/UserController.ts
@@ -2,23 +2,23 @@ import { Request, Response } from "express";
 import {User, validUser} from "../Model/User";
 
 export default class UserController{
-    async getAll(req: Request, res: Response): Promise<Object>{
+    async getAll(req: Request, res: Response): Promise<any>{
         const users = await User.find();
         if(!users) return res.status(404).send('Not Found');
 
         return res.send(users);
     }
 
-    async getById(req: Request, res: Response): Promise<Object>{
+    async getById(req: Request, res: Response): Promise<any>{
         const user = await User.findById(req.params.id);
         if(!user) return res.status(404).send('Not Found');
 
         return res.send(user);
     }
 
-    async createUser(req: Request, res: Response): Promise<Object>{
+    async createUser(req: Request, res: Response): Promise<any>{
         const result = await validUser(req.body);
-        if(result.error) return res.status(403).send('Not Valid data');
+        if(result.error) return res.status(400).send('Not Valid data');
 
         const user = new User({
             username: req.body.username,
@@ -27,5 +27,22 @@ export default class UserController{
 
         await user.save();
         return res.send(user);
+    }
+
+    async editUser(req: Request, res: Response): Promise<any>{
+        const findUser = await User.findById(req.params.id);
+        if(!findUser) return res.status(404).send('User not found');
+
+        const result = await validUser(req.body);
+        if(result.error) return res.status(404).send('Not Valid Data');
+
+        const user = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                username: req.body.username,
+                password: req.body.password
+            }
+        }, {new: true})
+
+        res.send(user);
     }
 }
